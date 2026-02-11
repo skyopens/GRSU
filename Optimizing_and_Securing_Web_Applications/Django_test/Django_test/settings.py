93f1f75114245f832239426930cdff17e39c4269
_HT_ <hantzhou@HanTdeMacBook-Pro.local>
2026-02-11 10:50:56 +0300

    Completed practical No.4

--- Optimizing_and_Securing_Web_Applications/Django_test/Django_test/settings.py
+++ Optimizing_and_Securing_Web_Applications/Django_test/Django_test/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h%k$(q-t!b)u8+k#&17im=wb*#)2lj6q!5xhy86*(p&d4mk(vi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
@@ -116,3 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
