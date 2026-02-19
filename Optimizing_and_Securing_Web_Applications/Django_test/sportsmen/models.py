84de325f07443190a31b71a41473b023561187fd
_HT_ <hantzhou@52-140-57-86.cisco-wifi.beltelecom.by>
2026-02-19 11:43:58 +0300

    Completed practical No.5

--- Optimizing_and_Securing_Web_Applications/Django_test/sportsmen/models.py
+++ Optimizing_and_Securing_Web_Applications/Django_test/sportsmen/models.py
@@ -8,3 +8,6 @@ class Sportsman(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.title} | {self.content} | {self.is_published} | {self.time_update}"
