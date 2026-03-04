7d6c9b8a027b52dfe2f30f1688ed673cbabe58d2
_HT_ <hantzhou@HanTdeMacBook-Pro.local>
2026-03-04 14:51:09 +0300

    Completed practical No.8

--- Optimizing_and_Securing_Web_Applications/Django_test/sportsmen/admin.py
+++ Optimizing_and_Securing_Web_Applications/Django_test/sportsmen/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Sportsman, Sports
 
 # Register your models here.
+class SportsmanAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'time_create')
+
+admin.site.register(Sportsman, SportsmanAdmin)
+
+class SportsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    
+admin.site.register(Sports, SportsAdmin)
+admin.site.site_url = '/sportsmen/'
--- Optimizing_and_Securing_Web_Applications/Django_test/sportsmen/models.py
+++ Optimizing_and_Securing_Web_Applications/Django_test/sportsmen/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 # Create your models here.
 class Sportsman(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name='Full Name')
     content = models.TextField(blank=True)
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
     time_create = models.DateTimeField(auto_now_add=True)
@@ -11,14 +11,29 @@ class Sportsman(models.Model):
     is_published = models.BooleanField(default=True)
     sport = models.ForeignKey('Sports', on_delete=models.CASCADE, null=True)
 
-    def __str__(self):
-        return f"{self.title} | {self.content} | {self.is_published} | {self.time_update}"
+    class Meta:
+        verbose_name = "Sport news"
+        verbose_name_plural = "Sport news"
+        ordering = ['-title']   
+
+    # def __str__(self):
+    #     return f"{self.title} | {self.content} | {self.is_published} | {self.time_update}"
     
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.id})
+
+    # def get_absolute_url(self):
+    #     return reverse('home')
+    
+    def __str__(self):
+        return self.title
     
 class Sports(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, db_index=True, verbose_name="The name of sport")
+
+    class Meta:
+        verbose_name = "Sport"
+        ordering = ['-name']   
 
     def __str__(self):
         return self.name
